34994770a539a555ad488c7229b2547c86ef5841
N.Nobuaki <149361480+nnobuaki0625@users.noreply.github.com>
2024-01-23 13:08:03 +0900

    Add files via upload

---
+++ apexChart.js
@@ -0,0 +1,66 @@
+
+let list;
+const jsonFilePath = 'edit.json';
+
+fetch(jsonFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        list = data;
+
+        /// ApexChartsに適合する配列に変換
+const ohlcList = []
+for (let i = 0; i < list.length; ++i) {
+    const rawOhlc = list[i]
+    ohlcList.unshift({
+        x: new Date(rawOhlc['date']),
+        y: [
+            rawOhlc['open'],
+            rawOhlc['high'],
+            rawOhlc['low'],
+            rawOhlc['close']
+        ]
+    })
+}
+
+    // オプションの記述
+const options = {
+    series: [{
+        data: ohlcList
+    }],
+    chart: {
+        type: 'candlestick',
+        height: 350
+    },
+    title: {
+        text: 'USDJPY Chart',
+        align: 'left'
+    },
+    xaxis: {
+        type: 'datetime'
+    },
+    yaxis: {
+        tooltip: {
+            enabled: true
+        }
+    }
+}
+
+// 最終出力の記述
+const chart = new ApexCharts(
+    document.querySelector("#chart"), options
+)
+chart.render()
+
+
+
+
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
